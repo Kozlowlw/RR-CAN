5933ff195cb8b5b9d1e06e4dd924220183c02cea
Kozlowlw <kozlowlw@rose-hulman.edu>
2018-04-19 16:16:01 -0400

    Just an archive of the project
    Archive of the ada.rose-hulman.edu gitlab project.
    This project was sponsored by Rolls Royce as our Senior Project. I worked on the CHT and CHT2 files independantly for the most part as most of my job in the group was to research vulnerabilities. CHT was build on socketCAN while CHT2 was built on from pyvit. CHT was dropped a few weeks in for a preference towards the ease of use of pyvit. The only non-working part of CHT2/hack is the UDS and IsoTP portions because of a time constraint. The grapher.py file is also old, the newest revision was lost but as this is an archive I will not be redoing the file.

---
+++ Code/ECU/Arduino/new_rpm/new_rpm.ino
@@ -0,0 +1,139 @@
+#include <mcp_can.h>
+#include <SPI.h>
+
+#define CAN0_INT 2
+#define CS 10
+
+#define SEND_DELAY  50
+#define CYCLE_DELAY 2
+
+#define DEBUG_MODE  1
+#define HARDENED    1
+
+MCP_CAN CAN0(CS);
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+bool receivedmsg;
+
+bool ID_CONFLICT = false;
+
+int sendtimer = SEND_DELAY;
+
+int sensorPin = A0;
+int brakePin = A1;
+
+int errorID = 0x001;
+
+const int rpmID = 0x00C;
+
+
+int getRpmData() {
+  return analogRead(sensorPin) * 12;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Ready.");
+
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  while(!(CAN_OK == CAN0.begin(CAN_500KBPS))) {
+    if(CAN0.begin(CAN_500KBPS) == CAN_OK) 
+      Serial.println("MCP2515 Initialized Successfully!");
+    else Serial.println("Error Initializing MCP2515...");
+  }
+  
+  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+  //attachInterrupt(CAN0_INT, MCP2515_ISR, FALLING); // start interrupt
+}
+
+void loop()
+{
+  if (CAN0.checkReceive()) {
+    receivemsg();
+    if (ID_CONFLICT && HARDENED) {
+      Serial.println("Someone else is using our id...?");
+      senderror();
+    }
+  }
+  if (sendtimer == 0) {
+    sendData();
+    sendtimer = SEND_DELAY;
+  }
+  sendtimer--;
+  delay(CYCLE_DELAY);
+}
+
+void receivemsg() {
+  receivedmsg = false;
+  ID_CONFLICT = false;
+  if(!digitalRead(CAN0_INT)) {           // If CAN0_INT pin is low, read receive buffer
+    CAN0.readMsgBufID(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    
+    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    else
+      sprintf(msgString, "Standard ID: 0x%.3lX  ID: %d Our ID: %d DLC: %1d  Data:", rxId, rxId, rpmID, len);
+    
+    Serial.print(msgString);
+    
+    if((rxId & 0x40000000) == 0x40000000) {    // Determine if message is a remote request frame.
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++) {
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
+      }
+    }
+    Serial.println(rpmID == rxId);
+
+    if (rxId == rpmID)
+      ID_CONFLICT = true;
+
+    receivedmsg = true;
+  }
+}
+
+void senderror() {
+  char data[2];
+  data[1] = (rpmID & 0xFF00) >> 8;
+  data[0] = rpmID & 0xFF;
+
+  bool success = sendmsg(errorID, data, 2);
+  if (true) {
+    if(success) {
+      Serial.println("Sent temperature successfully");
+    } else {
+      Serial.println("Error in sending temperature...");
+    }
+  }
+}
+
+void sendData() {
+  char data[1];
+  bool success;
+
+  int rpm = getRpmData();
+  data[1] = (rpm & 0xFF00) >> 8;
+  data[0] = rpm & 0x00FF;
+
+  success = sendmsg(rpmID, data, 1);
+
+  if(success) {
+    Serial.println("Sent data successfully");
+  } else {
+    Serial.println("Error in sending data...");
+  }
+}
+
+bool sendmsg(int CAN_ID, char* buf, int len) {
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(CAN_ID, 0, len, (const byte*) buf);
+  if(sndStat == CAN_OK){
+    return true;
+  }
+  return false;
+}
---
+++ Code/ECU/Arduino/speed_cont_node/speed_cont_node.ino
@@ -0,0 +1,196 @@
+#include <mcp_can.h>
+#include <SPI.h>
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+bool receivedmsg;
+
+#define CAN0_INT 2
+#define CS 10
+
+#define DEBUG_MODE 1
+
+int sensorPin = A0;
+int brakePin = A1;
+
+int errorID = 0x001;
+
+int speedID = 0x00D;
+int brakeID = 0x0FC;
+
+bool RECV_INT_FLAG = false;
+bool RESPONSE_REQUESTED = false;
+bool ID_CONFLICT = false;
+
+MCP_CAN CAN0(CS);
+
+#define MODE_BUTTON 3
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Ready.");
+
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  while(!(CAN_OK == CAN0.begin(CAN_500KBPS))) {
+    if(CAN0.begin(CAN_500KBPS) == CAN_OK) 
+      Serial.println("MCP2515 Initialized Successfully!");
+    else Serial.println("Error Initializing MCP2515...");
+  }
+  
+  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+  pinMode(MODE_BUTTON, INPUT_PULLUP);
+  
+  attachInterrupt(CAN0_INT, MCP2515_ISR, FALLING); // start interrupt
+}
+
+byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+int pModeButton = true;
+bool sendMode = true;
+bool pSendMode;
+
+void loop()
+{
+  int modeButton = digitalRead(MODE_BUTTON);
+  if(modeButton == LOW && pModeButton){
+    if (DEBUG_MODE) Serial.println("Mode changed.");
+    sendMode = !sendMode;
+  }
+
+  pSendMode = sendMode;
+  if(RECV_INT_FLAG) {
+    sendMode = false;
+    RECV_INT_FLAG = false;
+  }
+
+  if(sendMode || RESPONSE_REQUESTED) {
+    sendSensorData();
+    Serial.println("Sent data!");
+    delay(100);   // send data per 100ms
+  } else {
+    receivemsg(); // but always listen in receive mode
+    if(receivedmsg && pSendMode) {
+      sendMode = true;
+    }
+
+    if(ID_CONFLICT) {
+      Serial.println("Our ID was detected!! Error!!");
+      sendError();
+      Serial.println("Sent an error message");
+    }
+  }
+
+  pModeButton = modeButton;
+}
+
+int getSpeedData() {
+  return analogRead(sensorPin) / 4;
+}
+
+int getBrakeData() {
+  return digitalRead(brakePin);
+}
+
+void MCP2515_ISR()
+{
+    RECV_INT_FLAG = true;
+}
+
+void sendSensorData() {
+  char data[1];
+  data[0] = getSpeedData();
+  bool success = sendmsg(speedID, data, 1);
+  data[0] = getBrakeData();
+  success = success && sendmsg(brakeID, data, 1);
+  if (true) {
+    if(success) {
+      Serial.println("Sent temperature successfully");
+    } else {
+      Serial.println("Error in sending temperature...");
+    }
+  }
+}
+
+void sendSpeedData() {
+  char data[1];
+  data[0] = getSpeedData();
+  bool success = sendmsg(speedID, data, 1);
+  if (DEBUG_MODE) {
+    if(success) {
+      Serial.println("Sent speed successfully");
+    } else {
+      Serial.println("Error in sending speed...");
+    }
+  }
+}
+
+void sendBrakeData() {
+  char data[1];
+  data[0] = (char)getBrakeData();
+  bool success = sendmsg(brakeID, data, 1);
+  if (DEBUG_MODE) {
+    if(success) {
+      Serial.println("Sent brake info successfully");
+    } else {
+      Serial.println("Error in sending brake info...");
+    }
+  }
+}
+
+void sendError() {
+  char data[2];
+  data[1] = 0x01;
+  data[0] = 0x01;
+
+  bool success = sendmsg(errorID, data, 2);
+  if (true) {
+    if(success) {
+      Serial.println("Sent temperature successfully");
+    } else {
+      Serial.println("Error in sending temperature...");
+    }
+  }
+}
+
+bool sendmsg(int CAN_ID, char* buf, int len) {
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(CAN_ID, 0, len, (const byte*) buf);
+  if(sndStat == CAN_OK){
+    return true;
+  } 
+  return false;
+}
+
+void receivemsg() {
+  receivedmsg = false;
+  ID_CONFLICT = false;
+  if(!digitalRead(CAN0_INT)) {           // If CAN0_INT pin is low, read receive buffer
+    CAN0.readMsgBufID(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    
+    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    else
+      sprintf(msgString, "Standard ID: 0x%.3lX  DLC: %1d  Data:", rxId, len);
+    
+    Serial.print(msgString);
+    
+    if((rxId & 0x40000000) == 0x40000000) {    // Determine if message is a remote request frame.
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      RESPONSE_REQUESTED = true;
+      Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++) {
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
+      }
+    }
+    Serial.println();
+
+    if((rxId && 0xFFFF) == speedID || (rxId && 0xFFFF) == brakeID)
+      ID_CONFLICT = true;
+
+    receivedmsg = true;
+  }
+}
